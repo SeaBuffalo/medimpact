9bc4bc27a52a67ef6ce7e492feba8cdbafcb4d2a
Ben <104328460+bdos201@users.noreply.github.com>
2023-05-22 08:02:56 -0700

    initial ui layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PrescriptionsComponent } from './components/prescriptions/prescriptions.component';
+import { SearchComponent } from './components/search/search.component';
+import { PrescriptionComponent } from './components/prescription/prescription.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PrescriptionsComponent,
+    SearchComponent,
+    PrescriptionComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
